b082ab1af2c85bc5b093c94f34fd8515b96df883
dhruvaindavarapu <idhruva@horizon.csueastbay.edu>
2024-01-17 17:11:01 -0800

    Finished Week-6 assignments

--- week-6/1-use-memo/src/App.jsx
+++ week-6/1-use-memo/src/App.jsx
@@ -4,11 +4,12 @@ import { Assignment2 } from './components/Assignment2'
 import { Assignment3 } from './components/Assignment3'
 
 function App() {
+  console.log('App component is called!')
   return (
     <>
       <Assignment1 />
-      {/* <Assignment2 /> */}
-      {/* <Assignment3 /> */}
+      <Assignment2 />
+      <Assignment3 />
     </>
   )
 }
